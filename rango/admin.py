83398ae236ad3cf4b48711404a61803c42587abb
Fin Gray <2677288g@student.gla.ac.uk>
2023-02-05 14:49:47 +0000

    chap 9 tests complete

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from rango.models import Category, Page, UserProfile
-
+from rango.models import Category, Page
+from rango.models import UserProfile
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
--- rango/forms.py
+++ rango/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-from rango.models import Page, Category, UserProfile
 from django.contrib.auth.models import User
+from rango.models import Page, Category, UserProfile
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
@@ -33,7 +33,7 @@ class PageForm(forms.ModelForm):
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
-    
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password',)
